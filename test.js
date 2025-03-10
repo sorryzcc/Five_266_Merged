c4b0775829785537a19bd12f9ad8bb4886a604ac
zhangcongcong <66811734+sorryzcc@users.noreply.github.com>
2025-03-10 21:52:26 +0800

    修改读取文件名字

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const XLSX = require('xlsx');
 
 // 文件路径定义
-const excelpath = './key_266文本_ms文本_266负责人_ms负责人_266来源.xlsm';
+const excelpath = './key_266文本_ms文本_266负责人_ms负责人_266来源.xlsx';
 
 // 读取 Excel 文件
 function readExcel(filePath, fileName) {
